6405131a925423c18d4878122745c558bca09eb5
NrjMeyer <37902950+NrjMeyer@users.noreply.github.com>
2018-05-23 07:55:23 +0300

    Final

--- js/main.js
+++ js/main.js
@@ -57,21 +57,35 @@
     function result() {
      var k = [];
      var l = [];
+     btmEgal();
 
      a = document.getElementById('calc-output').innerHTML;
      for (var i=0; i < a.length; i++) {
         if (a[i] != "+" && a[i] != "-" && a[i] != "*" && a[i]!= "/" ){
                 k.push(parseInt(a[i]));
                 var m = k.join("");
-                alert(m);
+                //alert(m);
                 
             }
         else {
-            r = calculate(parseInt(m),a[i],parseInt(a[i+1]));
-            alert(r);
-            k=[];
-        }    
-
+            j=i+1;
+            if (a[j] != "+" && a[j] != "-" && a[j] != "*" && a[j]!= "/" ){
+                l.push(parseInt(a[j]));
+                r = calculate(parseInt(m),a[i],parseInt(l));
+                //alert(r);
+                i=i+1;
+            }
+            else {
+                r = calculate(parseInt(m),a[i-1],parseInt(l));
+                alert(r);
+                m=r;
+                k=[];
+                l=[];
+                i+=1;
+            }   
+            i+=1;
         }
+    }
+        return r;
      
     } 
